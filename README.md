e28ded02bb72ffbefd03d9e14bfd97b2d3bd9952
Hsuanyin <61041593+hsuan81@users.noreply.github.com>
2022-08-24 00:07:12 +0800

    create recipe backend using nest.js

---
+++ README.md
@@ -0,0 +1,22 @@
+# Installation
+
+Install Nest.js globally and prisma
+
+```
+npm install install -g @nestjs/cli
+cd recipe-app
+npm install prisma --save-dev
+```
+invoke Prisma CLI locally
+
+```
+npx prisma
+```
+
+# Dev with Docker
+Test without authentication
+
+```
+npm run start:dev
+```
+
---
+++ recipe-app/src/cats/cats.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { ListAllEntities } from './dto/list-all-entities';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
