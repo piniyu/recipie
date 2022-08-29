9be82ace0000b50881088af692e423b9c9dc55b5
Hsuanyin <61041593+hsuan81@users.noreply.github.com>
2022-08-30 01:02:30 +0800

    created schema

--- recipe-app/src/app.module.ts
+++ recipe-app/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CatsController } from './cats/cats.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: false,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
