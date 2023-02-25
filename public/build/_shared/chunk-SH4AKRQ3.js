2ea0f86a670b4eab189cc6687cb5515f72a0d931
Ching Chen <piniyu699@gmail.com>
2023-02-26 01:13:32 +0800

    delete build

--- public/build/_shared/chunk-SH4AKRQ3.js
+++
@@ -1,7 +0,0 @@
-// public/assets/img1.jpeg
-var img1_default = "/build/_assets/img1-L5MTI6MD.jpeg";
-
-export {
-  img1_default
-};
-//# sourceMappingURL=/build/_shared/chunk-SH4AKRQ3.js.map
--- public/build/_shared/chunk-TW2OTEET.js
+++
@@ -1,1 +0,0 @@
-var r="/build/_assets/img1-L5MTI6MD.jpeg";export{r as a};
